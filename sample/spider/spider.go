c14d0afccd8442ccb80b41513f522d4f2d8c3ea1
ｚ <xja@users.noreply.github.com>
2019-01-09 08:43:42 +0800

    Update spider.go

--- sample/spider/spider.go
+++ sample/spider/spider.go
@@ -7,6 +7,11 @@ import (
 	"github.com/shiyanhui/dht"
 	"net/http"
 	_ "net/http/pprof"
+	"database/sql"
+    _ "github.com/ziutek/mymysql/godrv"
+	//"strings"
+	"strconv"
+	"time"
 )
 
 type file struct {
@@ -43,25 +48,52 @@ func main() {
 				InfoHash: hex.EncodeToString(resp.InfoHash),
 				Name:     info["name"].(string),
 			}
-
+			
+			// total file size
+			var total_length uint64 = 0
 			if v, ok := info["files"]; ok {
 				files := v.([]interface{})
 				bt.Files = make([]file, len(files))
-
+				
 				for i, item := range files {
 					f := item.(map[string]interface{})
 					bt.Files[i] = file{
 						Path:   f["path"].([]interface{}),
 						Length: f["length"].(int),
 					}
+					total_length += uint64(f["length"].(int))
 				}
 			} else if _, ok := info["length"]; ok {
 				bt.Length = info["length"].(int)
 			}
 
-			data, err := json.Marshal(bt)
+			files_json, err := json.Marshal(bt.Files)
 			if err == nil {
-				fmt.Printf("%s\n\n", data)
+				//fmt.Println(bt.Files)
+				con, err := sql.Open("mymysql", "magnet" + "/" + "user" + "/" + "pass")
+                if err != nil {
+                        panic(err)
+                    }
+                    // runs before return
+                defer con.Close()
+                    // check if already exists
+                var id = 0
+                con.QueryRow("select id from hash_table where infohash=? limit 1", bt.InfoHash).Scan( & id)
+                    // _, err = con.Exec("insert into hash_table (infohash, name, length) VALUEs (\"cb6a91da42636fe9dbf1316b11621d7a642752d8\", \"Daisy_Stone.1080.mp4\", 1109711913)")
+                if id == 0 {
+					// fmt.Printf("%#v\n\n%#v\n\n%#v\n\n%#v\n\n\n",  bt.InfoHash, bt.Name, bt.Files, bt.Length)
+                    if len(bt.Files) == 0 {
+                        //_, err := 
+						con.Exec("insert into hash_table (infohash, name, length, len_h, date, file_count) values (?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, bt.Length, len_h_calc(uint64(bt.Length)), time.Now().Format("2006-01-02"), 1)
+						// fmt.Println(err)
+                    } else {
+						_, err := con.Exec("insert into hash_table (infohash, name, length, files, file_count, len_h, date) values (?, ?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, total_length, files_json, len(bt.Files), len_h_calc(total_length), time.Now().Format("2006-01-02"))
+						fmt.Println(err)
+                    }
+					fmt.Println(bt.Name + "\n\n")
+                }
+			}else{
+				fmt.Println(err)
 			}
 		}
 	}()
@@ -74,4 +106,31 @@ func main() {
 	d := dht.New(config)
 
 	d.Run()
+}
+
+func len_h_calc(len uint64) string{
+	var l float32
+	s := ""
+	if len < 1024 {
+		s = strconv.FormatUint(len, 10) + "Bytes"
+	}else{
+		if (len / 1024) < 1024 {
+			l = float32(len) / 1024
+			s = fmt.Sprintf("%.2f", l) + "KiB"
+		}else{
+			if (len / 1024 / 1024) < 1024 {
+				l = float32(len) / 1024 / 1024
+				s = fmt.Sprintf("%.2f", l) + "MiB"
+			}else{
+				if (len / 1024 / 1024 / 1024) < 1024 {
+					l = float32(len) / 1024 / 1024 / 1024
+					s = fmt.Sprintf("%.2f", l) + "GiB"
+				}else{
+					l = float32(len) / 1024 / 1024 / 1024 / 1024
+					s = fmt.Sprintf("%.2f", l) + "TiB"
+				}
+			}
+		}
+	}
+	return s
 }
