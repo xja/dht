4f8fa0a5228b12548e7c09cdef9e438633365756
ｚ <xja@users.noreply.github.com>
2019-01-13 22:45:00 +0800

    Update spider.go

--- sample/spider/spider.go
+++ sample/spider/spider.go
@@ -12,6 +12,8 @@ import (
 	//"strings"
 	"strconv"
 	"time"
+	"log"
+	"os"
 )
 
 type file struct {
@@ -70,28 +72,50 @@ func main() {
 			files_json, err := json.Marshal(bt.Files)
 			if err == nil {
 				//fmt.Println(bt.Files)
-				con, err := sql.Open("mymysql", "magnet" + "/" + "user" + "/" + "pass")
+				con, err := sql.Open("mymysql", "magnet" + "/" + "torrent" + "/" + "ndBEwdl26Q-iuxIQ")
                 if err != nil {
                         panic(err)
                     }
                     // runs before return
-                defer con.Close()
-                    // check if already exists
-                var id = 0
-                con.QueryRow("select id from hash_table where infohash=? limit 1", bt.InfoHash).Scan( & id)
+                //defer con.Close()
+                    // check if already exists, use insert ignore instead check before inserting
+                // var id = 0
+                // con.QueryRow("select id from hash_table where infohash=? limit 1", bt.InfoHash).Scan( & id)
                     // _, err = con.Exec("insert into hash_table (infohash, name, length) VALUEs (\"cb6a91da42636fe9dbf1316b11621d7a642752d8\", \"Daisy_Stone.1080.mp4\", 1109711913)")
-                if id == 0 {
+					// append if exists, otherwise create and write only
+				logFile, err := os.OpenFile("spider.log", os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0777)
+				logger:=log.New(logFile,"\r\n", log.Ldate)
+                /* if id == 0 {
 					// fmt.Printf("%#v\n\n%#v\n\n%#v\n\n%#v\n\n\n",  bt.InfoHash, bt.Name, bt.Files, bt.Length)
                     if len(bt.Files) == 0 {
-                        //_, err := 
-						con.Exec("insert into hash_table (infohash, name, length, len_h, date, file_count) values (?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, bt.Length, len_h_calc(uint64(bt.Length)), time.Now().Format("2006-01-02"), 1)
-						// fmt.Println(err)
+                        _, err := con.Exec("insert delayed into hash_table (infohash, name, length, len_h, date, file_count) values (?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, bt.Length, len_h_calc(uint64(bt.Length)), time.Now().Format("2006-01-02"), 1)
+						if err != nil{
+							logger.Printf("%s\n%s\n%s\n",err.Error() ,bt.InfoHash, bt.Name)
+						}
                     } else {
-						_, err := con.Exec("insert into hash_table (infohash, name, length, files, file_count, len_h, date) values (?, ?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, total_length, files_json, len(bt.Files), len_h_calc(total_length), time.Now().Format("2006-01-02"))
-						fmt.Println(err)
+						_, err := con.Exec("insert delayed into hash_table (infohash, name, length, files, file_count, len_h, date) values (?, ?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, total_length, files_json, len(bt.Files), len_h_calc(total_length), time.Now().Format("2006-01-02"))
+						if err != nil{
+							logger.Printf("%s\n%s\n%s\n",err.Error() ,bt.InfoHash, bt.Name)
+						}
                     }
-					fmt.Println(bt.Name + "\n\n")
-                }
+					fmt.Println(bt.Name + "\n")
+                } */ // end of if id == 0
+				
+				if len(bt.Files) == 0 {
+					_, err := con.Exec("insert ignore into hash_table (infohash, name, length, len_h, date, file_count) values (?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, bt.Length, len_h_calc(uint64(bt.Length)), time.Now().Format("2006-01-02"), 1)
+					//fmt.Println(bt.Name + "\n")
+					if err != nil{
+						logger.Printf("%s\n%s\n%s\n",err.Error() ,bt.InfoHash, bt.Name)
+					}
+				} else {
+					_, err := con.Exec("insert ignore into hash_table (infohash, name, length, files, file_count, len_h, date) values (?, ?, ?, ?, ?, ?, ?)", bt.InfoHash, bt.Name, total_length, files_json, len(bt.Files), len_h_calc(total_length), time.Now().Format("2006-01-02"))
+					//fmt.Println(bt.Name + "\n")
+					if err != nil{
+						logger.Printf("%s\n%s\n%s\n",err.Error() ,bt.InfoHash, bt.Name)
+					}
+				}
+				con.Close()
+				logFile.Close()
 			}else{
 				fmt.Println(err)
 			}
